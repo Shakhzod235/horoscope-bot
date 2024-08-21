04277a80ec4112712758fe69c9534cbbfb25f742
Shakhzod235 <shakhzodikramov235@gmail.com>
2024-08-21 16:02:02 +0500

    feat: implement back button

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,15 +1,35 @@
 const tg = window.Telegram.WebApp;
 
-export function useTelegram() {
+export function useTelegram(navigate) {
   const onClose = () => {
     tg.close();
   };
   const expand = () => {
     tg.expand();
   };
+  const showBackButton = () => {
+    tg.BackButton.show();
+  };
+  const hideBackButton = () => {
+    tg.BackButton.hide();
+  };
+  const handleBackButtonClick = () => {
+    navigate(-1);
+  };
+  const BackButtonOnClick = (callback) => {
+    tg.BackButton.onClick(callback);
+  };
+  const BackButtonOffClick = (callback) => {
+    tg.BackButton.offClick(callback);
+  };
   return {
+    tg,
     onClose,
     expand,
-    tg,
+    showBackButton,
+    hideBackButton,
+    handleBackButtonClick,
+    BackButtonOnClick,
+    BackButtonOffClick,
   };
 }
--- src/pages/Description.jsx
+++ src/pages/Description.jsx
@@ -1,6 +1,28 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import DescriptionContent from "../components/DescriptionContent";
+import { useTelegram } from "../hooks/useTelegram";
 
 export default function Description() {
+  const navigate = useNavigate();
+  const {
+    showBackButton,
+    hideBackButton,
+    handleBackButtonClick,
+    BackButtonOnClick,
+    BackButtonOffClick,
+  } = useTelegram(navigate);
+
+  useEffect(() => {
+    showBackButton();
+
+    BackButtonOnClick(handleBackButtonClick);
+
+    return () => {
+      hideBackButton();
+      BackButtonOffClick(handleBackButtonClick);
+    };
+  }, []);
   return (
     <>
       <DescriptionContent />
